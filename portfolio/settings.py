633bc5126a4cc7c7753118e33f4330306e45f07f
Deibys <atom32@tutanota.com>
2022-05-31 10:32:54 -0400

    whitenoise configured

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,8 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -151,3 +153,7 @@ CORS_ALLOW_METHODS = [
     'POST',
     'PUT',
 ]
+
+# Whitenoise
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
